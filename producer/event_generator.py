c25105319b47a2240375e6c832a1583550e17775
Kamran Akhtar <kamranakthar8@gmail.com>
2026-08-31 00:53:14 +0530

    some commit event gen to test in termux

--- producer/event_generator.py
+++ producer/event_generator.py
@@ -8,19 +8,19 @@ import time
 from datetime import datetime
 from kafka import KafkaProducer
 
-# Kafka Configuration Done Here
-KAFKA_TOPIC = "userevents"
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer = lambda v: json.dumps(v).encode('utf-8')
-)
-
+# # Kafka Configuration Done Here
+# KAFKA_TOPIC = "userevents"
+# producer = KafkaProducer(
+#     bootstrap_servers=['localhost:9092'],
+#     value_serializer = lambda v: json.dumps(v).encode('utf-8')
+# )
+app = FastAPI(title="E-commerce Dummy DATA API")
 event_type = ["order_created","order_received","order_shipped","order_processing","order_cancelled","order_return"]
 city_name = ["Mumbai","Delhi","Bengaluru","Hyderabad","Chennai","Kolkata","Ahmedabad","Pune","Jaipur","Surat","Lucknow","Kanpur","Nagpur","Indore","Bhopal","Patna","Vadodara","Visakhapatnam","Coimbatore","Chandigarh","Kochi","Guwahati","Varanasi"]
 
 def generate_ecommerce_event():
-    while True:
-        event = {
+    # while True:
+        return event = {
             "event_id": f"evt_{random.randint(10000, 99999)}",
             "user_id": f"usr_{random.randint(100, 999)}",
             "product_id": f"prd_{random.randint(100, 999)}",
@@ -29,13 +29,35 @@ def generate_ecommerce_event():
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "city": random.choice(city_name)
         }
-        yield event
+        # yield event
 
-for event in generate_ecommerce_event():
-    producer.send(KAFKA_TOPIC, value=event)
-    if event['event_type'] in ['created', 'done']:
-        producer.send('order_events', value=event)
-    elif event['event_type'] == 'received':
-        producer.send('payment_events', value=event)
-    print(f"Produced: {event}")
-    time.sleep(1)
+
+
+@app.get("/")
+def home():
+    return {
+        "status" : "running",
+        "message" : "E-Commerce API is running"
+    }
+
+@app.get("/event")
+def get_event():
+    return generate_ecommerce_event()
+
+
+@app.get("/events")
+def get_events(count: int = 10):
+    count = min(count,100)
+    return [
+        generate_ecommerce_event()
+        for _ in range(count)
+    ]
+
+# for event in generate_ecommerce_event():
+#     producer.send(KAFKA_TOPIC, value=event)
+#     if event['event_type'] in ['created', 'done']:
+#         producer.send('order_events', value=event)
+#     elif event['event_type'] == 'received':
+#         producer.send('payment_events', value=event)
+#     print(f"Produced: {event}")
+#     time.sleep(1)
